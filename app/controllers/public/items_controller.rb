402711a317f636092187deb863ee2828e56acb15
Eugene.K <67733078+EugeneKawamoto@users.noreply.github.com>
2020-09-21 18:45:23 +0900

    Revert "Develop"

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,11 @@
 class Public::ItemsController < ApplicationController
 
 def index
-  @items = Item.all.page(params[:page]).per(8) #kaminari記述
+  
 end
 
 def show
-  @item = Item.find(params[:id])
-  @cart_item = CartItem.new
+  
 end
 
 
