c21e1f93ea269625368bc8dd8b7888d65065b7fe
Eugene Kawamoto <eugenenzv8@gmail.com>
2020-09-22 11:32:49 +0000

    add order_confirm_fix_index, fix model

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,4 +4,8 @@ class Delivery < ApplicationRecord
   validates :postal_code, presence: true, format: { with: /\A\d{7}\z/, message: 'ハイフンなし7桁で入力してください。'}
   validates :address, presence: true
   validates :name, presence: true
+
+  def full_delivery
+    "#{self.postal_code} #{self.address} #{self.name}"
+  end
 end
