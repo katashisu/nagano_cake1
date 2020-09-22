57e52e25a325ea945ac05433d3e973827f29b06a
shibata07 <hsryzgzgentil49@gmail.com>
2020-09-22 11:46:55 +0000

    add cart_item/update

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,6 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+
+  validates :amount, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 30} #1~30の整数
 end
